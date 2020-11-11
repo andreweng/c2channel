d71e679353863347f67b0d73ae692bd477363c6b
Andrew <andrewceng@gmail.com>
2020-11-11 16:03:06 +0700

    synched via script

--- chat.py
+++ chat.py
@@ -106,7 +106,7 @@ async def on_message(message):
         # Kill existing c2 channels
         chat_pid = os.system("ps aux | grep 'chat' | grep -v grep | awk -F '  ' '{print $2}'")
         kill_chat = 'kill -9 ' + str(chat_pid)
-        git_update = 'git pull https://github.com/andreweng/c2channel.git')
+        git_update = 'git pull https://github.com/andreweng/c2channel.git'
 
         await message.channel.send(f'--- Hey, I will be right back, I am going to do some updates...')
         stdout = Popen(git_update, shell=True, stdout=PIPE).stdout
