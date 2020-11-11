632bab29ed59b727ef5ef96bb4aa97027d9abaf0
Andrew <andrewceng@gmail.com>
2020-11-11 20:31:49 +0700

    synched via script

--- sync.sh
+++ sync.sh
@@ -3,11 +3,13 @@
 touch chat.log &&
 touch command_dm.log &&
 touch nohup.out &&
+touch command.log && 
 
 # Remove logs
 rm chat.log &&
 rm command_dm.log &&
 rm nohup.out &&
+rm command.log &&
 
 # Synch up github
 git add -A &&
