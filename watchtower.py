4f43d0f245d8ced795066c64907961537987bf3c
Andrew <andrewceng@gmail.com>
2020-11-11 11:01:04 +0700

    added watchtower - respawn processes

---
+++ watchtower.py
@@ -0,0 +1,10 @@
+import os, time, logging
+
+cmd = "ps aux | grep 'chat.py' | grep -v grep"
+
+while True: 
+    retcode = os.system(cmd)
+    if retcode:
+        os.system('nohup /usr/bin/python3 /etc/c2chan/chat.py &')
+        logging.warn('...Disconnected from c2 channel\n Restarting chat.py')
+    time.sleep(10)
